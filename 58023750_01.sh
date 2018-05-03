f611d4a089f78e0166b0b3bc7aa345bce412a3b1
chaosnerd <mafianook@hotmail.com>
2018-05-03 14:02:20 +0700

    asdf

---
+++ 58023750_01.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+who
+echo "Script  Starting"
+mkdir quiz
+cd quiz
+mkdir backup
+mkdir getup
+cd getup
+for((i=0;i<=10;i+=2));
+do
+    touch test$i
+done
+pwd >> test2
+
+tar cvfz ../backup/bkup.tar.gz test*
+cd getup
+rm *0
+cd ..
+tar xvfz backup/bkup.tar.gz -C ../quiz
+cp test0 getup/
+mv backup/bkup.tar.gz ../quiz
+rm -r backup
+rm -r getup
+mv test2 output
+ls > summary
+cd ..
+mv quiz quiz1
+echo "Script  End"
+
+
